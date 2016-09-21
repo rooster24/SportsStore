c1b5a2c5aeb733d078a649d4c39b1e9f5b0c3750
troy <troy@MAINPC3>
2016-09-21 13:39:23 -0500

    Added some js

---
+++ SportsStore.WebUI/scripts/workingScript1.js
@@ -0,0 +1,16 @@
+﻿function checkLength(e, minLength) {
+    var el, elMsg;
+    el = e.target;
+    elMsg = document.getElementById('nameMsg');
+
+    if (el.value.length < minLength) {
+        elMsg.innerHTML = 'You must enter at least ' + minLength + ' characters or more!';
+    } else {
+        elMsg.innerHTML = '';
+    }
+}
+
+var elUsername = document.getElementById('firstName');
+elUsername.addEventListener('blur', function(e) {
+  checkLength(e, 5);
+}, false);
